7e753ccd12759be1f5c90ed7cda1543c908d38d3
Daniel Randall <dgrandall@me.com>
2019-09-16 15:08:23 -0600

    working on testing

---
+++ lazarette/lazarette_test.go
@@ -0,0 +1,317 @@
+package lazarette
+
+import (
+	context "context"
+	"math/rand"
+	"os"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	proto "github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
+	grpc "google.golang.org/grpc"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" + "!@#$%^&*()-_=+;|/\\{}"
+
+var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func startServer(tb testing.TB, address string) *Server {
+	server, err := NewServer(os.TempDir())
+	if err != nil {
+		tb.Fatalf("failed to start server: %v", err)
+	}
+
+	err = server.Clean()
+	if err != nil {
+		tb.Fatalf("failed to clean server: %v", err)
+	}
+
+	go func() {
+		err = server.StartGRPCServer(address)
+		if err != nil {
+			tb.Fatalf("failed to start grpc server: %v", err)
+		}
+	}()
+
+	return server
+}
+
+func closeServer(tb testing.TB, s *Server) {
+	err := s.Close()
+	if err != nil {
+		tb.Fatalf("failed to close server: %v", err)
+	}
+}
+
+func cleanServer(tb testing.TB, s *Server) {
+	err := s.Clean()
+	if err != nil {
+		tb.Fatalf("failed to clean server: %v", err)
+	}
+}
+
+func newClient(tb testing.TB, address string) LazaretteClient {
+	// conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		tb.Fatalf("failed to connect to server: %v", err)
+	}
+
+	return NewLazaretteClient(conn)
+}
+
+func randKey(tb testing.TB, maxLength int) *Key {
+	for {
+		b := make([]byte, seededRand.Intn(maxLength))
+		for i := range b {
+			b[i] = charset[seededRand.Intn(len(charset))]
+		}
+
+		if len(string(b)) > 0 {
+			return &Key{
+				Key: string(b),
+			}
+		}
+	}
+}
+
+func randVal(tb testing.TB, maxLength int) *Value {
+	buf := make([]byte, seededRand.Intn(maxLength))
+	_, err := seededRand.Read(buf)
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	return &Value{
+		Timestamp: ptypes.TimestampNow(),
+		Data:      buf,
+	}
+}
+
+func checkValueEqual(tb testing.TB, key *Key, expected, actual *Value) {
+	if !proto.Equal(expected, actual) {
+		tb.Fatalf("values don't match for key %q:\n\texpected: %s\n\tactual: %s\n", key.GetKey(), expected.String(), actual.String())
+	}
+}
+
+func setAndCheck(tb testing.TB, client LazaretteClient, kv *KeyValue) {
+	_, err := client.Set(context.Background(), kv)
+	if err != nil {
+		tb.Fatalf("failed to set %q: %v. buf was 0x%x", kv.GetKey().GetKey(), err, kv.GetValue().GetData())
+	}
+
+	nval, err := client.Get(context.Background(), kv.GetKey())
+	if err != nil {
+		tb.Fatalf("failed to get %q: %v", kv.GetKey().GetKey(), err)
+	}
+
+	checkValueEqual(tb, kv.GetKey(), kv.GetValue(), nval)
+}
+
+func TestSetAndGet(t *testing.T) {
+	server := startServer(t, "localhost:7777")
+	client := newClient(t, "localhost:7777")
+
+	kv := &KeyValue{
+		Key:   randKey(t, 50),
+		Value: randVal(t, 300),
+	}
+
+	setAndCheck(t, client, kv)
+	closeServer(t, server)
+}
+
+func TestSettingTheSameKey(t *testing.T) {
+	server := startServer(t, "localhost:7777")
+	client := newClient(t, "localhost:7777")
+
+	kv := &KeyValue{
+		Key:   randKey(t, 50),
+		Value: randVal(t, 300),
+	}
+
+	for i := 0; i < 10; i++ {
+		setAndCheck(t, client, kv)
+		kv.Value = randVal(t, 300)
+	}
+
+	closeServer(t, server)
+}
+
+func TestConcurrentSettingTheSameKey(t *testing.T) {
+	server := startServer(t, "localhost:7777")
+
+	kv := &KeyValue{
+		Key:   randKey(t, 50),
+		Value: randVal(t, 300),
+	}
+
+	wg := sync.WaitGroup{}
+	test := func(tt *testing.T, n int) {
+		go func() {
+			defer wg.Done()
+			client := newClient(tt, "localhost:7777")
+
+			for i := 0; i < n; i++ {
+				time.Sleep(time.Duration(seededRand.Intn(500)) * time.Millisecond)
+				kv.GetValue().Timestamp = ptypes.TimestampNow()
+
+				_, err := client.Set(context.Background(), kv, grpc.WaitForReady(true))
+				if err != nil && !strings.Contains(err.Error(), ErrNotNew.Error()) {
+					tt.Fatalf("failed to set %q: %v. buf was 0x%x", kv.GetKey().GetKey(), err, kv.GetValue().GetData())
+				}
+			}
+		}()
+	}
+
+	t.Run("32Routines", func(tt *testing.T) {
+		for i := 0; i < 32; i++ {
+			wg.Add(1)
+			go test(tt, 8)
+		}
+
+		wg.Wait()
+	})
+
+	cleanServer(t, server)
+
+	t.Run("64Routines", func(tt *testing.T) {
+		for i := 0; i < 64; i++ {
+			wg.Add(1)
+			go test(tt, 16)
+		}
+
+		wg.Wait()
+	})
+
+	cleanServer(t, server)
+
+	t.Run("128Routines", func(tt *testing.T) {
+		for i := 0; i < 128; i++ {
+			wg.Add(1)
+			go test(tt, 16)
+		}
+
+		wg.Wait()
+	})
+
+	closeServer(t, server)
+}
+
+func TestConcurrentSettingRandomKeys(t *testing.T) {
+	server := startServer(t, "localhost:7777")
+
+	wg := sync.WaitGroup{}
+	test := func(tt *testing.T, n int) {
+		go func() {
+			defer wg.Done()
+			client := newClient(t, "localhost:7777")
+
+			for i := 0; i < n; i++ {
+				time.Sleep(time.Duration(seededRand.Intn(500)) * time.Millisecond)
+
+				kv := &KeyValue{
+					Key:   randKey(tt, 50),
+					Value: randVal(tt, 300),
+				}
+
+				_, err := client.Set(context.Background(), kv, grpc.WaitForReady(true))
+				if err != nil && !strings.Contains(err.Error(), ErrNotNew.Error()) {
+					tt.Fatalf("failed to set %q: %v. buf was 0x%x", kv.GetKey().GetKey(), err, kv.GetValue().GetData())
+				}
+			}
+		}()
+	}
+
+	t.Run("32Routines", func(tt *testing.T) {
+		for i := 0; i < 32; i++ {
+			wg.Add(1)
+			go test(tt, 8)
+		}
+
+		wg.Wait()
+	})
+
+	cleanServer(t, server)
+
+	t.Run("64Routines", func(tt *testing.T) {
+		for i := 0; i < 64; i++ {
+			wg.Add(1)
+			go test(tt, 16)
+		}
+
+		wg.Wait()
+	})
+
+	cleanServer(t, server)
+
+	t.Run("128Routines", func(tt *testing.T) {
+		for i := 0; i < 128; i++ {
+			wg.Add(1)
+			go test(tt, 16)
+		}
+
+		wg.Wait()
+	})
+
+	closeServer(t, server)
+}
+
+func BenchmarkSet(b *testing.B) {
+	server := startServer(b, "localhost:7777")
+	client := newClient(b, "localhost:7777")
+
+	// generate keys/values
+	var keys []*Key
+	var vals []*Value
+
+	for i := 0; i < 1000; i++ {
+		keys = append(keys, randKey(b, 50))
+	}
+
+	for i := 0; i < 1000; i++ {
+		vals = append(vals, randVal(b, 300))
+	}
+
+	b.Run("UniqueKeys", func(bb *testing.B) {
+		for i := 0; i < bb.N; i++ {
+			val := vals[0]
+			val.Timestamp = ptypes.TimestampNow()
+
+			setAndCheck(bb, client, &KeyValue{
+				Key:   keys[i],
+				Value: val,
+			})
+		}
+	})
+
+	b.Run("UniqueVals", func(bb *testing.B) {
+		for i := 0; i < bb.N; i++ {
+			val := vals[i]
+			val.Timestamp = ptypes.TimestampNow()
+
+			setAndCheck(bb, client, &KeyValue{
+				Key:   keys[0],
+				Value: val,
+			})
+		}
+	})
+
+	b.Run("UniqueKeysAndVals", func(bb *testing.B) {
+		for i := 0; i < bb.N; i++ {
+			val := vals[i]
+			val.Timestamp = ptypes.TimestampNow()
+
+			setAndCheck(bb, client, &KeyValue{
+				Key:   keys[i],
+				Value: val,
+			})
+		}
+	})
+
+	closeServer(b, server)
+}
--- lazarette/server.go
+++ lazarette/server.go
@@ -4,8 +4,10 @@ import (
 	context "context"
 	"errors"
 	fmt "fmt"
+	"io"
 	"net"
 	"path/filepath"
+	"strings"
 	"sync"
 	"time"
 
@@ -14,10 +16,11 @@ import (
 	proto "github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	empty "github.com/golang/protobuf/ptypes/empty"
+	bolt "go.etcd.io/bbolt"
 	grpc "google.golang.org/grpc"
 )
 
-//go:generate protoc -I ../lazarette --go_out=plugins=grpc:../lazarette ../lazarette/lazarette.proto
+//go:generate protoc -I ./ --go_out=plugins=grpc:./ ./lazarette.proto
 
 var (
 	// ErrKeyNotFound .
@@ -27,32 +30,23 @@ var (
 	ErrNotNew = errors.New("a newer value was found; not setting")
 )
 
-type lazaretteServer struct {
-	store store.Store
+// Server .
+type Server struct {
+	store  store.Store
+	server *grpc.Server
 
 	subsMu sync.RWMutex
-	subs   map[string][]chan *pb.KeyValue
+	subs   map[string][]chan *KeyValue
 }
 
-// StartGRPCServer .
-func StartGRPCServer(laz pb.LazaretteServer, address string) error {
-	lis, err := net.Listen("tcp", address)
-	if err != nil {
-		return fmt.Errorf("failed to listen: %v", err)
-	}
-
-	grpcServer := grpc.NewServer()
-	pb.RegisterLazaretteServer(grpcServer, laz)
-	err = grpcServer.Serve(lis)
-	if err != nil {
-		return fmt.Errorf("failed to serve: %v", err)
-	}
-
-	return nil
+// Replication .
+type Replication struct {
+	KeyPrefix  string `json:"key-prefix"`
+	RemoteAddr string `json:"remote-addr"`
 }
 
 // NewServer .
-func NewServer(path string) (pb.LazaretteServer, error) {
+func NewServer(path string, repls ...Replication) (*Server, error) {
 	path = filepath.Clean(path)
 	options := &bolt.Options{
 		Timeout: 2 * time.Second,
@@ -68,16 +62,37 @@ func NewServer(path string) (pb.LazaretteServer, error) {
 		return nil, fmt.Errorf("unable to create boltstore: %v", err)
 	}
 
-	s := &lazaretteServer{
+	s := &Server{
 		store: store,
-		subs:  make(map[string][]chan *pb.KeyValue),
+		subs:  make(map[string][]chan *KeyValue),
+	}
+
+	for _, repl := range repls {
+		s.AddReplication(context.TODO(), repl)
 	}
 
 	return s, nil
 }
 
+// StartGRPCServer .
+func (s *Server) StartGRPCServer(address string) error {
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("failed to listen: %v", err)
+	}
+
+	s.server = grpc.NewServer()
+	RegisterLazaretteServer(s.server, s)
+	err = s.server.Serve(lis)
+	if err != nil {
+		return fmt.Errorf("failed to serve: %v", err)
+	}
+
+	return nil
+}
+
 // Get .
-func (s *lazaretteServer) Get(ctx context.Context, key *pb.Key) (*pb.Value, error) {
+func (s *Server) Get(ctx context.Context, key *Key) (*Value, error) {
 	if key == nil {
 		return nil, errors.New("key must not be nil")
 	}
@@ -90,7 +105,7 @@ func (s *lazaretteServer) Get(ctx context.Context, key *pb.Key) (*pb.Value, erro
 		return nil, ErrKeyNotFound
 	}
 
-	val := &pb.Value{}
+	val := &Value{}
 	err = proto.Unmarshal(data, val)
 	if err != nil {
 		return nil, fmt.Errorf("unable to unmarshal val: %v", err)
@@ -100,23 +115,114 @@ func (s *lazaretteServer) Get(ctx context.Context, key *pb.Key) (*pb.Value, erro
 }
 
 // Set .
-func (s *lazaretteServer) Set(ctx context.Context, kv *pb.KeyValue) (*empty.Empty, error) {
+func (s *Server) Set(ctx context.Context, kv *KeyValue) (*empty.Empty, error) {
+	err := s.set(ctx, kv)
+	if err != nil {
+		return nil, err
+	}
+
+	// notify all subscriptions that this key has updated
+	s.subsMu.RLock()
+	for prefix, chans := range s.subs {
+		if strings.HasPrefix(kv.GetKey().GetKey(), prefix) {
+			for i := range chans {
+				chans[i] <- kv
+			}
+		}
+	}
+	s.subsMu.RUnlock()
+
+	return &empty.Empty{}, nil
+}
+
+// Subscribe .
+func (s *Server) Subscribe(prefix *Key, stream Lazarette_SubscribeServer) error {
+	if prefix == nil {
+		return errors.New("prefix must not be nil")
+	}
+
+	ch := make(chan *KeyValue)
+	s.subsMu.Lock()
+	s.subs[prefix.GetKey()] = append(s.subs[prefix.GetKey()], ch)
+	s.subsMu.Unlock()
+
+	for kv := range ch {
+		err := stream.Send(kv)
+		if err != nil {
+			// TODO print error
+		}
+	}
+
+	return nil
+}
+
+// AddReplication .
+func (s *Server) AddReplication(ctx context.Context, repl Replication) error {
+	conn, err := grpc.Dial(repl.RemoteAddr)
+	if err != nil {
+		return fmt.Errorf("unable to open connection: %v", err)
+	}
+	defer conn.Close()
+
+	client := NewLazaretteClient(conn)
+	prefix := &Key{
+		Key: repl.KeyPrefix,
+	}
+
+	stream, err := client.Subscribe(ctx, prefix)
+	if err != nil {
+		return fmt.Errorf("unable to subscribe: %v", err)
+	}
+
+	for {
+		kv, err := stream.Recv()
+		switch {
+		case kv == nil:
+			continue
+		case err == io.EOF:
+			break
+		case err != nil:
+			return fmt.Errorf("unable to recv message from stream: %v", err)
+		}
+
+		err = s.set(ctx, kv)
+		if err != nil && !errors.Is(err, ErrNotNew) {
+			// log something?
+		}
+	}
+}
+
+// Close .
+func (s *Server) Close() error {
+	if s.server != nil {
+		s.server.GracefulStop()
+	}
+
+	return s.store.Close()
+}
+
+// Clean .
+func (s *Server) Clean() error {
+	return s.store.Clean()
+}
+
+func (s *Server) set(ctx context.Context, kv *KeyValue) error {
 	switch {
 	case kv == nil:
-		return nil, errors.New("kv must not be nil")
-	case kv.GetValue() == nil:
-		return nil, errors.New("value must not be nil")
-	case kv.GetValue().GetTimestamp() == nil:
-		return nil, errors.New("timestamp must not be nil")
+		return errors.New("kv must not be nil")
 	case kv.GetKey() == nil:
-		return nil, errors.New("key must not be nil")
+		return errors.New("key must not be nil")
+	case kv.GetValue() == nil:
+		return errors.New("value must not be nil")
+	case kv.GetValue().GetTimestamp() == nil:
+		return errors.New("timestamp must not be nil")
 	}
 
 	// get the current val
 	cur, err := s.Get(ctx, kv.GetKey())
 	switch {
 	case err != nil && !errors.Is(err, ErrKeyNotFound):
-		return nil, fmt.Errorf("unable to get current value: %v", err)
+		return fmt.Errorf("unable to get current value: %v", err)
 	case cur == nil:
 		// shouldn't ever happen
 	}
@@ -124,54 +230,28 @@ func (s *lazaretteServer) Set(ctx context.Context, kv *pb.KeyValue) (*empty.Empt
 	if cur.GetTimestamp() != nil {
 		curTime, err := ptypes.Timestamp(cur.GetTimestamp())
 		if err != nil {
-			return nil, fmt.Errorf("unable to check current timestamp: %v", err)
+			return fmt.Errorf("unable to check current timestamp: %v", err)
 		}
 
 		valTime, err := ptypes.Timestamp(kv.GetValue().GetTimestamp())
 		if err != nil {
-			return nil, fmt.Errorf("unable to check timestamp: %v", err)
+			return fmt.Errorf("unable to check timestamp: %v", err)
 		}
 
 		if valTime.Before(curTime) {
-			return nil, ErrNotNew
+			return ErrNotNew
 		}
 	}
 
-	bytes, err := proto.Marshal(kv)
+	bytes, err := proto.Marshal(kv.GetValue())
 	if err != nil {
-		return nil, fmt.Errorf("unable to marshal new value: %v", err)
+		return fmt.Errorf("unable to marshal new value: %v", err)
 	}
 
 	err = s.store.Put([]byte(kv.GetKey().GetKey()), bytes)
 	if err != nil {
-		return nil, fmt.Errorf("unable to put value into store: %v", err)
+		return fmt.Errorf("unable to put value into store: %v", err)
 	}
 
-	/*
-		keyval := cache.KeyValue{
-			Key:   key,
-			Value: &val,
-		}
-
-		// notify all subscriptions that this key has updated
-		l.subsMu.RLock()
-		for prefix, chans := range l.subs {
-			if strings.HasPrefix(key, prefix) {
-				for i := range chans {
-					chans[i] <- keyval
-				}
-			}
-		}
-		l.subsMu.RLock()
-	*/
-
-	return nil, nil
-}
-
-// Subscribe .
-func (s *lazaretteServer) Subscribe(prefix *pb.Key, stream pb.Lazarette_SubscribeServer) error {
 	return nil
-}
-
-func main() {
 }
