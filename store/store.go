a7154a8fcd938589d4fa6719d100b76d75de66cb
Daniel Randall <dgrandall@me.com>
2019-10-10 14:14:50 -0600

    get prefix on syncmapstore

--- store/store.go
+++ store/store.go
@@ -3,11 +3,14 @@ package store
 // Store .
 type Store interface {
 	Get(key []byte) ([]byte, error)
+	GetPrefix(prefix []byte) ([]KeyValue, error)
 	Set(key, val []byte) error
 
 	Close() error
 	Clean() error
+}
 
-	// DumpPrefix()
-	// Dump() (map[[]byte][]byte, error)
+type KeyValue struct {
+	Key   []byte
+	Value []byte
 }
--- store/syncmapstore/syncmap_test.go
+++ store/syncmapstore/syncmap_test.go
@@ -38,7 +38,7 @@ func TestSet(t *testing.T) {
 
 	err := store.Set(key, val)
 	if err != nil {
-		t.Fatalf("failed to put key: %v", err)
+		t.Fatalf("failed to set %q: %v", key, err)
 	}
 
 	nval, err := store.Get(key)
@@ -73,7 +73,7 @@ func TestClean(t *testing.T) {
 
 	err := store.Set(key, val)
 	if err != nil {
-		t.Fatalf("failed to put key: %v", err)
+		t.Fatalf("failed to set %q: %v", key, err)
 	}
 
 	err = store.Clean()
@@ -87,5 +87,59 @@ func TestClean(t *testing.T) {
 		t.Fatalf("failed to get key: %v", err)
 	case nval != nil:
 		t.Fatalf("expected: nil\ngot: 0x%x", nval)
+	}
+}
+
+func TestGetPrefix(t *testing.T) {
+	store := newStore(t)
+	defer store.Close()
+
+	key1 := []byte("ITB-1101-CP1")
+	val1 := []byte(`{"key1": "val1"}`)
+
+	key2 := []byte("ITB-1101-CP2")
+	val2 := []byte(`{"key2": "val2"}`)
+
+	key3 := []byte("ITB-2202-CP3")
+	val3 := []byte(`{"key3": "val3"}`)
+
+	err := store.Set(key1, val1)
+	if err != nil {
+		t.Fatalf("failed to set %q: %s", key1, err)
+	}
+
+	err = store.Set(key2, val2)
+	if err != nil {
+		t.Fatalf("failed to set %q: %s", key2, err)
+	}
+
+	err = store.Set(key3, val3)
+	if err != nil {
+		t.Fatalf("failed to set %q: %s", key3, err)
+	}
+
+	prefix := []byte("ITB-1101")
+	kvs, err := store.GetPrefix(prefix)
+	if err != nil {
+		t.Fatalf("failed to get prefix %q: %s", prefix, err)
+	}
+
+	if len(kvs) != 2 {
+		t.Fatalf("expected %d keys, but got %d\nkvs: %+v", 2, len(kvs), kvs)
+	}
+
+	for _, kv := range kvs {
+		switch {
+		case bytes.Equal(key1, kv.Key):
+			if !bytes.Equal(val1, kv.Value) {
+				t.Fatalf("in key %q:\nexpected 0x%x\ngot: 0x%x", kv.Key, val1, kv.Value)
+			}
+		case bytes.Equal(key2, kv.Key):
+			if !bytes.Equal(val2, kv.Value) {
+				t.Fatalf("in key %q:\nexpected 0x%x\ngot: 0x%x", kv.Key, val2, kv.Value)
+			}
+		default:
+			t.Fatalf("expected %q and %q in response, but got %q", key1, key2, kv.Key)
+		}
 	}
 }
